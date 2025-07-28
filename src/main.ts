7e13aa2aef96276633e5e66e62bfed0ddf0093cd
Yusuke Kawamoto <novogrammer@gmail.com>
2025-07-28 19:54:27 +0900

    three.js導入

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,51 @@
+import { getElementSize } from './dom_utils';
 import './style.scss'
+
+import * as THREE from 'three/webgpu';
+
+
+async function mainAsync(){
+  const backgroundElement=document.querySelector<HTMLHtmlElement>(".p-background");
+
+  if(!backgroundElement){
+    throw new Error("backgroundElement is null");
+  }
+
+  const {width,height}=getElementSize(backgroundElement);
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+  const renderer = new THREE.WebGPURenderer({
+    // forceWebGL:true,
+  });
+  renderer.setSize( width, height );
+  renderer.setAnimationLoop( animate );
+  renderer.domElement.classList.add("p-background__canvas");
+  backgroundElement.appendChild( renderer.domElement );
+
+
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  function animate(){
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+  }
+
+}
+
+
+
+mainAsync().catch((error)=>{
+  console.error(error);
+});
+
+
 
 
