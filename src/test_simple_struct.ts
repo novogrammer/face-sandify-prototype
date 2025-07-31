b84ec2083cd049602408ce579222f513bc137f94
Yusuke Kawamoto <novogrammer@gmail.com>
2025-07-31 14:31:03 +0900

    WebGL fallbackでも動く単純なstructを試す。

---
+++ src/test_simple_struct.ts
@@ -0,0 +1,19 @@
+import { float, Fn, struct, type ShaderNodeObject } from "three/tsl";
+import { NodeMaterial } from "three/webgpu";
+
+export function testSimpleStructNode(material:NodeMaterial){
+  const S=struct({
+    a:"float",
+    b:"float",
+  })
+  const simple=Fn(([a,b]:[ReturnType<typeof float>,ReturnType<typeof float>])=>{
+    const s=S({
+      a,
+      b,
+    });
+    const result = s.get("a").add(s.get("b"));
+    return result;
+  })
+  material.colorNode=simple(float(0.25),float(0.75));
+
+}
