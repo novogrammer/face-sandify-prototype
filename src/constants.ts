5c4b8cd9af32e4b794a1d376cb99b5c45516b64e
Yusuke Kawamoto <novogrammer@gmail.com>
2025-08-05 18:34:59 +0900

    解像度変更

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ENABLE_FORCE_WEBGL=false;
 export const SHOW_WGSL_CODE=false;
 
-export const SAND_SIMULATOR_WIDTH=128;
-export const SAND_SIMULATOR_HEIGHT=128;
+export const SAND_SIMULATOR_WIDTH=128*4;
+export const SAND_SIMULATOR_HEIGHT=128*4;
