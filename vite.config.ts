6f4ce7a73098112d057f808522d950d34a1ac0c6
Yusuke Kawamoto <novogrammer@gmail.com>
2025-08-18 20:24:56 +0900

    ngrok対応

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base:"./",
+  server:{
+    host: '0.0.0.0',
+    allowedHosts: [
+      '.ngrok-free.app',
+    ],    
+  },
 });
 
